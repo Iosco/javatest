50d8a5ac1e3ec307233c0a86fd7aa8dba8b52c3f
Andrew <48227299+googlefan2@users.noreply.github.com>
2020-09-05 20:12:48 -0400

    Add files via upload

---
+++ helptest.java
@@ -0,0 +1,33 @@
+
+/**
+ * this is quite frankly, a small test, to feel the ropes of Java and the BlueJ software. Nothing big, might put it on github to work that out as well. 
+ *
+ * 
+ * @version 0.0.1
+ */
+public class helptest
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class helptest
+     */
+    public helptest()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
